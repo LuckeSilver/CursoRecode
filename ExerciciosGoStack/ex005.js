c1cde27cea6ee0b7a7844d2b35338d7acd050489
Lucas Silva <luckeprogramer@gmail.com>
2019-09-30 11:15:14 -0300

    Conclusão dos exercicios GoStack

---
+++ ExerciciosGoStack/ex005.js
@@ -0,0 +1,13 @@
+var usuarios = [{
+    nome: "Lucas",
+    habilidades: ["Javascript", "React", "ReactNative"]
+},
+{
+    nome: "Karen",
+    habilidades: ["VueJs", "Ruby on Rails", "Elixir"]
+}
+];
+
+function skills() {
+    
+}
