3b433f656ed784bf6ce7be322260bd549a2190ca
LuckeSilver <37150645+LuckeSilver@users.noreply.github.com>
2019-10-17 15:59:07 -0500

    adicionando condições no formulário

--- Aula16-10/main.js
+++ Aula16-10/main.js
@@ -1,8 +1,12 @@
 
 function catchName () {
   let name = document.getElementById('nome').value;
-  if (name != "") {
-    alert(`Parabéns, ${name}, seu cadastro foi efetuado com Sucesso!`);
+  let sname = document.getElementById('snome').value;
+  let login = document.getElementById('login').value;
+  let password = document.getElementById('senha1').value;
+  let password2 = document.getElementById('senha2').value;
+  if (name != "" && sname != "" && login != "" && password != "" && password2 != "") {
+    alert(`Parabéns, ${name} ${sname}, seu cadastro foi efetuado com Sucesso!`);
     }
   }
 
