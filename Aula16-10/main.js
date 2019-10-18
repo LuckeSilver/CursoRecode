a124f5f57e6e917d1d47a9f40027674f20cb7c57
LuckeSilver <37150645+LuckeSilver@users.noreply.github.com>
2019-10-18 12:35:19 -0500

    atualização form todas as condições

--- Aula16-10/main.js
+++ Aula16-10/main.js
@@ -5,7 +5,10 @@ function catchName () {
   let login = document.getElementById('login').value;
   let password = document.getElementById('senha1').value;
   let password2 = document.getElementById('senha2').value;
-  if (name != "" && sname != "" && login != "" && password != "" && password2 != "") {
+  let email = document.getElementById('email').value;
+  let tel = document.getElementById('tel').value;
+  let endereco = document.getElementById('endereco').value;
+  if (name != "" && sname != "" && login != "" && password != "" && password2 != "" && email != "" && tel != "" && endereco != "") {
     alert(`Parabéns, ${name} ${sname}, seu cadastro foi efetuado com Sucesso!`);
     }
   }
